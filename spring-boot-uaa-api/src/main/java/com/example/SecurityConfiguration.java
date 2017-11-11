1f2b9bf564a4a3c25d3abdb265dae910a11e8241
zhuqinghua <zhuqinghua@emcc.net.cn>
2017-11-11 18:59:17 +0800

    Implicit Grant

--- spring-boot-uaa-api/src/main/java/com/example/SecurityConfiguration.java
+++ spring-boot-uaa-api/src/main/java/com/example/SecurityConfiguration.java
@@ -1,24 +1,8 @@
 package com.example;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
-@EnableOAuth2Sso
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http.antMatcher("/**").authorizeRequests()
-			.antMatchers("/","/api","/api/login**", "/webjars/**").permitAll()
-			.anyRequest().authenticated()
-			.and().logout().logoutSuccessUrl("/").permitAll()
-			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-		// @formatter:on
-	}
-
-}
+@EnableResourceServer
+public class SecurityConfiguration {}
